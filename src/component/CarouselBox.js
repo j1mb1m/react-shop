6b06ab106e0ab6563cc17879b4c30b88831a4067
Dasha Astakhova <dasha.kozhalo@gmail.com>
2022-12-16 01:22:15 +0300

    CHAGED IMG

--- src/component/CarouselBox.js
+++ src/component/CarouselBox.js
@@ -4,14 +4,14 @@ import Carousel from 'carousel-react-rcdev'
 function CarouselBox(props) {
     return (
         <Carousel>
-            <CategoryCard image="/react-shop/img/dress/dress1.jpg" title="Куртки" />
-            <CategoryCard image="/react-shop/img/dress/dress2.jpg" title="Пальто" />
-            <CategoryCard image="/react-shop/img/dress/dress3.jpg" title="Шубы" />
-            <CategoryCard image="/react-shop/img/dress/dress4.jpg" title="Парки" />
-            <CategoryCard image="/react-shop/img/dress/dress5.jpg" title="Тренчи" />
-            <CategoryCard image="/react-shop/img/dress/dress2.jpg" title="Пуховики" />
-            <CategoryCard image="/react-shop/img/dress/dress6.jpg" title="Костюмы" />
-            <CategoryCard image="/react-shop/img/dress/dress4.jpg" title="Жакеты" />
+            <CategoryCard image="/img/dress/dress1.jpg" title="Куртки" />
+            <CategoryCard image="/img/dress/dress2.jpg" title="Пальто" />
+            <CategoryCard image="/img/dress/dress3.jpg" title="Шубы" />
+            <CategoryCard image="/img/dress/dress4.jpg" title="Парки" />
+            <CategoryCard image="/img/dress/dress5.jpg" title="Тренчи" />
+            <CategoryCard image="/img/dress/dress2.jpg" title="Пуховики" />
+            <CategoryCard image="/img/dress/dress6.jpg" title="Костюмы" />
+            <CategoryCard image="/img/dress/dress4.jpg" title="Жакеты" />
         </Carousel>)
 }
 
--- src/component/Header.js
+++ src/component/Header.js
@@ -7,7 +7,7 @@ function Header() {
 
         <header>
           <div className="contentSize menu">
-            <img className="burger" src="/react-shop/img/burger.svg" alt="Burger" />
+            <img className="burger" src="img/burger.svg" alt="Burger" />
 
             <nav>
               <Link to='/'>New</Link>
@@ -15,17 +15,17 @@ function Header() {
               <Link to='/contacts'>О нас</Link>
             </nav>
 
-            <img className="logo" src="/react-shop/img/YANKI.svg" alt="Logo" />
+            <img className="logo" src="img/YANKI.svg" alt="Logo" />
 
             <ul className="countrySettings">
-              <li><div>Ru<img className="logo" src="/react-shop/img/drift_header.svg" alt="Logo" /></div>
+              <li><div>Ru<img className="logo" src="img/drift_header.svg" alt="Logo" /></div>
                 <ul>
                   <li>En</li>
                   <li>By</li>
                   <li>Ru</li>
                 </ul>
               </li>
-              <li>UAh<img className="logo" src="/react-shop/img/drift_header.svg" alt="Logo" />
+              <li>UAh<img className="logo" src="img/drift_header.svg" alt="Logo" />
                 <ul>
                   <li>USD</li>
                   <li>BYN</li>
@@ -35,10 +35,10 @@ function Header() {
             </ul>
 
             <ul className="userService">
-              <li><a href='/'><img src="/react-shop/img/search.svg" alt="Search" /></a></li>
-              <li><a href='/'><img src="/react-shop/img/user.svg" alt="User" /></a></li>
-              <li><a href='/'><img src="/react-shop/img/favorite.svg" alt="Favorite" /></a></li>
-              <li><a href='/'><img src="/react-shop/img/basket.svg" alt="Basket" /></a></li>
+              <li><a href='/'><img src="img/search.svg" alt="Search" /></a></li>
+              <li><a href='/'><img src="img/user.svg" alt="User" /></a></li>
+              <li><a href='/'><img src="img/favorite.svg" alt="Favorite" /></a></li>
+              <li><a href='/'><img src="img/basket.svg" alt="Basket" /></a></li>
             </ul>
 
           </div>
